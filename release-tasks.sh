89e622971dfea32c8ed82a56095e941fa6f23bfe
Pavel Bobylev <p_bobylev@bk.ru>
2021-06-26 16:13:40 +0300

    Go env

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,1 +1,2 @@
-docker run -v migrations:/migrations --network host migrate/migrate -path=/migrations/ -database ${DATABASE_URL} up
+go env
+#docker run -v migrations:/migrations --network host migrate/migrate -path=/migrations/ -database ${DATABASE_URL} up

40c71565f08b1337c8a7f5b1ad26ed8127c61097
Pavel Bobylev <p_bobylev@bk.ru>
2021-06-26 16:33:18 +0300

    build with tags

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,4 +1,6 @@
-ls -l
-ls -l bin
+ls -la
+ls -la bin
 make build
+ls -la
+ls -la build
 #docker run -v migrations:/migrations --network host migrate/migrate -path=/migrations/ -database ${DATABASE_URL} up
